ee7175dcf8329dc5ec9914613e0c97bf451b4b18
Chris Bacon <chrisbacon2009@hotmail.com>
2018-12-21 10:07:05 +0000

    added mkBackup and swapFiles

--- _functions.sh
+++ _functions.sh
@@ -1,11 +1,10 @@
-
-reinstallStack(){
+reinstallStack() {
     rm -rf ~/.stack
     rm -rf /use/local/bin/stack
     curl -sSL https://get.haskellstack.org/ | sh
 }
 
-runTest(){
+runTest() {
     if [ -z "$1" ]; then
         echo "I need a module name"
     else
@@ -13,14 +12,32 @@ runTest(){
     fi
 }
 
-makeGPGKey(){
+makeGPGKey() {
    gpg --default-new-key-algo rsa4096 --gen-key 
 }
 
-encryptFile(){
+encryptFile() {
     if [ -z "$1" ]; then
         echo "I need an email and a file name"
     else
         gpg -e -r $1 $2
+    fi
+}
+
+mkBackup() {
+    if [ -z "$1" ]; then
+        echo "I need a filename"
+    else
+        cp -r $1 $1.bk.$(date +%s)
+    fi
+}
+
+swapFiles() {
+    if [ -z "$1" ] && [ -z "$2" ]; then
+        echo "I two files to swap"
+    else
+        mv $1 $1.tmp
+        mv $2 $1
+        mv $1.tmp $2
     fi
 }
