82d5b9a8b463dec5793cdfb75f2e18aa2eea0f82
Chris Bacon <chrisbacon2009@hotmail.com>
2019-02-01 11:53:50 +0000

    Update install.sh

--- .config/install.sh
+++ .config/install.sh
@@ -83,6 +83,18 @@ function install_tmux_config() {
     cp .tmux/.tmux.conf.local .
 }
 
+function install_polybar() {
+    sudo apt install build-essential git cmake cmake-data pkg-config libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
+    cd ~
+    git clone --recursive https://github.com/jaagr/polybar
+    cd polybar
+    mkdir build
+    cd build
+    cmake ..
+    make -j$(nproc)
+    sudo make install
+}
+
 function exit_msg() {
     echo -e """
         .___                    
