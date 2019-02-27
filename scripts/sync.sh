efaaf174d9fc03b58f8ff3212018b941e877650c
chris-bacon <chrisbacon2009@hotmail.com>
2019-02-27 10:40:02 +0000

    Automated sync of /home/chrisbacon/scripts

---
+++ scripts/sync.sh
@@ -0,0 +1,20 @@
+export DISPLAY=:0
+
+CONFIG=~/.config
+
+POLYBAR=$CONFIG/polybar/
+SCRIPT=~/scripts
+
+/usr/bin/notify-send 'Syncing...!' 'Automated sync in progress' --icon=dialog-information
+
+git add ~/.gitignore
+git commit -m "Automated sync of ~/.gitignore"
+
+cd $POLYBAR
+git add config
+git commit -m "Automated sync of $POLYBAR"
+
+cd $SCRIPT
+git add sync.sh
+git commit -m "Automated sync of $SCRIPT"
+git push origin master
