53f4b3e8ba171df64a98774c6fa9f692f40ba461
chris-bacon <chrisbacon2009@hotmail.com>
2019-03-12 10:45:04 +0000

    Automated sync of /home/chrisbacon/.config/nvim

--- .config/nvim/nvim.hs
+++
@@ -1,9 +0,0 @@
-import Neovim
-
-import qualified Neovim.Example.Plugin as Example
-
-main :: IO ()
-main = do
-    neovim defaultConfig
-        { plugins = plugins defaultConfig ++ [ Example.plugin ]
-        }
