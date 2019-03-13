4afc0aec956b8618e66da8b6e135d0de5a816d39
chris-bacon <chrisbacon2009@hotmail.com>
2019-03-13 21:40:05 +0000

    Automated sync of /home/chrisbacon/.config/nvim

--- .config/nvim/nvim-hs/HaskellFormatImport/Plugin.hs
+++ .config/nvim/nvim-hs/HaskellFormatImport/Plugin.hs
@@ -1,12 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 module HaskellFormatImport.Plugin (haskellFormatImport) where
 
+import Data.List
 import Data.Maybe
 import Neovim
 import Neovim.API.String
 
+-- echo HaskellFormatImport(expand('%:p'))
 haskellFormatImport :: String -> Neovim env String
 haskellFormatImport n = do
-    a <- liftIO $ readFile n
-    return a
+    fileContent <- liftIO $ readFile n
+
+    let splitIntoLInes = lines fileContent
+        imports = filter isImportStatement splitIntoLInes
+
+    -- let res = formatImport <$> lines a
+
+    return res
+
+isImportStatement :: String -> Bool
+isImportStatement = isInfixOf "import"
+
+-- formatImport :: String -> String
+-- formatImport s = 
 
