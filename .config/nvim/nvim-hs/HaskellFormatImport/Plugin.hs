993475d0501296454a02ba1464a8509fc9b34882
chris-bacon <chrisbacon2009@hotmail.com>
2019-03-13 15:40:04 +0000

    Automated sync of /home/chrisbacon/.config/nvim

--- .config/nvim/nvim-hs/HaskellFormatImport/Plugin.hs
+++ .config/nvim/nvim-hs/HaskellFormatImport/Plugin.hs
@@ -1,8 +1,14 @@
 module HaskellFormatImport.Plugin (haskellFormatImport) where
 
 import Neovim
+import Neovim.API.String
+
+getWindow :: NvimObject Window
 
 -- | Neovim is not really good with big numbers, so we return a String here.
 haskellFormatImport :: Neovim env String
-haskellFormatImport = return "bob"
+haskellFormatImport = do
+    win <- getWindow
+    print $ show win
+    return "bob"
 
