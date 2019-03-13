4b4bc8c3f1f82b1ca7896358f22a79ca940d7ab7
chris-bacon <chrisbacon2009@hotmail.com>
2019-03-13 17:00:05 +0000

    Automated sync of /home/chrisbacon/.config/nvim

--- .config/nvim/nvim-hs/HaskellFormatImport.hs
+++ .config/nvim/nvim-hs/HaskellFormatImport.hs
@@ -7,6 +7,6 @@ import HaskellFormatImport.Plugin (haskellFormatImport)
 plugin :: Neovim () NeovimPlugin
 plugin = wrapPlugin Plugin
     { environment = ()
-    , exports     = [ $(command' 'haskellFormatImport) [CmdSync Sync, CmdRange WholeFile] ]
+    , exports     = [ $(command' 'haskellFormatImport) [CmdSync Async, CmdRange WholeFile] ]
     -- , exports     = [ $(function' 'haskellFormatImport) Sync ]
     }
--- .config/nvim/nvim-hs/HaskellFormatImport/Plugin.hs
+++ .config/nvim/nvim-hs/HaskellFormatImport/Plugin.hs
@@ -5,13 +5,14 @@ import Data.Maybe
 import Neovim
 import Neovim.API.String
 
-haskellFormatImport :: Neovim env Buffer
+haskellFormatImport :: CommandArguments -> Neovim env Int
 haskellFormatImport (CommandArguments _ range _ _) = do
 -- haskellFormatImport = do
-    b <- vim_get_current_buffer
-    liftIO $ print b
-    vim_out_write "bob"
+    -- b <- vim_get_current_buffer
+    -- liftIO $ print b
+    -- vim_out_write "bob"
     let (x,y) = fromMaybe (0,0) range
+    liftIO $ print y
     return y
 
 
