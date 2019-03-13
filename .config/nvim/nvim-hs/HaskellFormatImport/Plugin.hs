eb5747c2b02a87beccfd00d52b81b3f430ae45e3
chris-bacon <chrisbacon2009@hotmail.com>
2019-03-13 16:50:04 +0000

    Automated sync of /home/chrisbacon/.config/nvim

--- .config/nvim/nvim-hs/HaskellFormatImport/Plugin.hs
+++ .config/nvim/nvim-hs/HaskellFormatImport/Plugin.hs
@@ -4,9 +4,11 @@ module HaskellFormatImport.Plugin (haskellFormatImport) where
 import Data.Maybe
 import Neovim
 
-haskellFormatImport :: CommandArguments -> Neovim env env
+haskellFormatImport :: CommandArguments -> Neovim env Int
 haskellFormatImport (CommandArguments _ range _ _) = do
-    e <- ask
+    b <- vim_get_current_buffer
+    liftIO $ print b
     let (x,y) = fromMaybe (0,0) range
-    return e
+    return x
+
 
