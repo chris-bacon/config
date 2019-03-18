b412b2ab8fd4071ca035863aef0b16c30b9b1e1d
chris-bacon <chrisbacon2009@hotmail.com>
2019-03-18 16:55:04 +0000

    Automated sync of /home/chrisbacon/.config/nvim

--- .config/nvim/nvim-hs/HaskellFormatImport/Plugin.hs
+++ .config/nvim/nvim-hs/HaskellFormatImport/Plugin.hs
@@ -26,39 +26,41 @@ substitute (start,end) ptn replacement flags = vim_command
 
 
 -- TODO: as
--- TODO: alphabetical
 
 haskellFormatImport :: CommandArguments -> Neovim env ()
 haskellFormatImport (CommandArguments _ range _ _) = do
   let (a, b) = fromMaybe (0,0) range
   buff <- vim_get_current_buffer
   allLines <- nvim_buf_get_lines buff (intToInt64 a) (intToInt64 b) False
-  let allImportLinesUnsorted = filter isImportStatement (zip [1..b] allLines)
-      allImportLines         = sortImports allImportLinesUnsorted
-      anyImportIsQualified   = not . null $ filter isQualified allImportLines
-      maxLength              = foldr max 0 $ fmap (\(_,s) -> length s) allImportLines
+  let allImportLines       = sortImports $ filter isImportStatement (zip [1..b] allLines)
+      anyImportIsQualified = getQualification allImportLines
+      maxLineLength        = foldr max 0 $ fmap (\(_,s) -> length s) allImportLines
   -- nvim_buf_set_lines buff 0 0 False allImportLines
-
   -- nvim_buf_get_lines
-  mapM_ (formatImportLine buff anyImportIsQualified maxLength) allImportLines
 
-  -- mapM_ (\line -> substitute (0, 10) line (line ++ "bob") ["g"]) allImportLines
+  mapM_ (formatImportLine buff anyImportIsQualified maxLineLength) allImportLines
+
   return ()
 
-
-formatImportLine :: Buffer -> Qualified -> Int -> (Int, String) -> Neovim env ()
+formatImportLine :: Buffer -> Qualification -> Int -> (Int, String) -> Neovim env ()
 formatImportLine buff qualifiedImports longestImport (lineNo, lineContent) = buffer_set_line buff (intToInt64 lineNo) $ padContent lineContent qualifiedImports longestImport
 
-data Qualified = ImportIsQualified | ImportIsNotQualified
+data Qualification = Present | NotPresent
 
-padContent :: String -> Qualified -> Int -> String
-padContent content ImportIsNotQualified longestImport = content
-padContent content ImportIsQualified longestImport =
+getQualification :: [(Int, String)] -> Qualification
+getQualification xs = go $ filter isQualified xs
+  where
+    go [] = NotPresent
+    go _  = Present
+
+padContent :: String -> Qualification -> Int -> String
+padContent content NotPresent longestImport = content
+padContent content Present longestImport =
   if "qualified" `isInfixOf` content || "import         " `isInfixOf` content
      then content
      else concat $ "import          " : splitOn "import" content
 
-sortImports :: [String] -> [String]
+sortImports :: [(Int, String)] -> [(Int, String)]
 sortImports xs = zip (fmap fst xs) $ sort (fmap snd xs)
 
 isImportStatement :: (Int, String) -> Bool
@@ -66,18 +68,4 @@ isImportStatement (_, s) = isInfixOf "import " s
 
 isQualified :: (Int, String) -> Bool
 isQualified (_, s) = isInfixOf "qualified " s
-
--- padImports :: [T.Text] -> Bool -> [T.Text]
--- padImports i False = i
--- padImports i True  = fmap padQualifiedIfMissing i
-
--- padQualifiedIfMissing :: T.Text -> T.Text
--- padQualifiedIfMissing s = if isQualified s || alreadyPadded s
---                              then s
---                              else T.replace "import" "import          " s
-
--- alreadyPadded = T.isInfixOf $ T.replicate qualifiedPadLength " "
-
--- formatImport :: String -> String
--- formatImport s = 
 
