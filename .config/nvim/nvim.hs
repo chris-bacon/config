049d40d46a87059fa7c62545206c18318b69afa1
chris-bacon <chrisbacon2009@hotmail.com>
2019-03-29 14:50:04 +0000

    Automated sync of /home/chrisbacon/.config/nvim

--- .config/nvim/nvim.hs
+++ .config/nvim/nvim.hs
@@ -1,10 +1,9 @@
 import Neovim
 
 import qualified Neovim.Example.Plugin as Example
-import qualified HaskellFormatImport as HFI
 
 main :: IO ()
 main = do
     neovim defaultConfig
-        { plugins = plugins defaultConfig ++ [ Example.plugin, HFI.plugin ]
+        { plugins = plugins defaultConfig ++ [ Example.plugin ]
         }
